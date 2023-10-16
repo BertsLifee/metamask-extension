114afaeb05b597f24d798eda57468b7138bd9463
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2023-10-16 13:54:19 +0100

    e2e to check metrics of blockaid toggle on/off
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- ui/pages/settings/experimental-tab/experimental-tab.component.js
+++ ui/pages/settings/experimental-tab/experimental-tab.component.js
@@ -86,10 +86,9 @@ export default class ExperimentalTab extends PureComponent {
       this.props;
     this.context.trackEvent({
       category: MetaMetricsEventCategory.Settings,
-      event: 'Enabled/Disable security_alerts_enabled',
+      event: MetaMetricsEventName.SecurityAlertsEnabled,
       properties: {
-        action: 'Enabled/Disable security_alerts_enabled',
-        legacy_event: true,
+        enabled: newValue,
       },
     });
     setSecurityAlertsEnabled(newValue);
